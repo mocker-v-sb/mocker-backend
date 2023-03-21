81b8868ae60adccf79c2241f54d1d3ef3078aad0
Daria Eremina <darrriae@yandex-team.ru>
2023-03-22 00:15:10 +0300

    ???

--- gateway/src/main/scala/com/mocker/models/rest/common/KVPair.scala
+++ gateway/src/main/scala/com/mocker/models/rest/common/KVPair.scala
@@ -1,7 +1,7 @@
 package com.mocker.models.rest.common
 
 import com.mocker.rest.request.{KVPair => ProtoKVPair}
-import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder}
+import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder}
 
 case class KVPair(name: String, value: String) {
 
@@ -10,9 +10,16 @@ case class KVPair(name: String, value: String) {
   }
 }
 
+// todo: ???
+case class Helper(name: String, value: String)
+
+object Helper {
+  implicit val decoder: JsonDecoder[Helper] = DeriveJsonDecoder.gen[Helper]
+}
+
 object KVPair {
   implicit val encoder = DeriveJsonEncoder.gen[KVPair]
-  implicit val decoder = DeriveJsonDecoder.gen[KVPair]
+  implicit val decoder = JsonDecoder[Helper].map(h => KVPair(h.name, h.value))
 
   def fromProto(protoKVPair: ProtoKVPair): KVPair = {
     KVPair(protoKVPair.name, protoKVPair.value)
