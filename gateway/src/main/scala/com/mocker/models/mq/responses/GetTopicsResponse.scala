8f2587adec14686e8caa6d5023f1f1eac2f360a5
Shair Dilavar <59974771+teetotu@users.noreply.github.com>
2023-04-18 23:14:13 +0300

    move queue class

--- gateway/src/main/scala/com/mocker/models/mq/responses/GetTopicsResponse.scala
+++ gateway/src/main/scala/com/mocker/models/mq/responses/GetTopicsResponse.scala
@@ -1,34 +1,12 @@
 package com.mocker.models.mq.responses
 
-import com.mocker.models.mq.ScalaBrokerType
-import com.mocker.models.mq.responses.GetTopicsResponse.Queue
-import com.mocker.mq.mq_service.{GetTopicsResponse => ProtoGetTopicsResponse, Queue => ProtoQueue}
-import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder}
+import com.mocker.models.mq.Queue
+import com.mocker.mq.mq_service.{GetTopicsResponse => ProtoGetTopicsResponse}
+import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder}
 
 case class GetTopicsResponse(queues: Seq[Queue])
-
-// todo: ???
-case class Helper(name: String, value: String)
-
-object Helper {
-  implicit val decoder: JsonDecoder[Helper] = DeriveJsonDecoder.gen[Helper]
-}
-
 object GetTopicsResponse {
-  case class Queue(brokerType: String, topicName: String)
 
-  object Queue {
-
-    def fromMessage(message: ProtoQueue): Either[String, Queue] = {
-      ScalaBrokerType.fromMessage(message.brokerType) match {
-        case Right(sbt)  => Right(Queue(brokerType = sbt, topicName = message.topicName))
-        case Left(error) => Left(error)
-      }
-    }
-
-    implicit val encoder = DeriveJsonEncoder.gen[Queue]
-    implicit val decoder = DeriveJsonDecoder.gen[Queue]
-  }
 
   def fromMessage(message: ProtoGetTopicsResponse): Either[String, GetTopicsResponse] = {
     val queuesAndErrors = message.queues.map(q => Queue.fromMessage(q))
