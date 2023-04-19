406351313007eedd26f054bea4e7d09348508f94
Daria Eremina <darrriae@yandex-team.ru>
2023-04-19 11:01:24 +0300

    implement converters

--- rest/api/src/main/scala/com/mocker/rest/utils/HeadersUtils.scala
+++ rest/api/src/main/scala/com/mocker/rest/utils/HeadersUtils.scala
@@ -2,13 +2,14 @@ package com.mocker.rest.utils
 
 import com.mocker.rest.request.KVPair
 import zio.http.model.Headers
+import zio.http.model.Headers.Header
 
 object HeadersUtils {
 
   implicit class HeadersImplicits(private val headers: Headers) extends AnyVal {
 
     def toKVPairs: Seq[KVPair] = {
-      ???
+      headers.toList.map { case Header(key, value)  => KVPair(key.toString, value.toString) }
     }
   }
 
